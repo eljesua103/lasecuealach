9ccd61ca3bc5ca6370f1317e0bfc6c0f742673bc
eljesua103 <dairomag12@gmail.com>
2024-07-09 16:19:47 +0000

    Mundo guardado 2024-07-09

--- server.py
+++ server.py
@@ -10,7 +10,7 @@
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
-# eu		a      Europa (Frankfurt)
+# eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
